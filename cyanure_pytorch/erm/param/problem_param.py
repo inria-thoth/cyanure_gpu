317162c16d5d0bad21b2c99c891a45006a3ed061
Thomas Ryckeboer <thomasryck@users.noreply.github.com>
2024-04-26 16:41:50 +0200

    optim: change lambda to tensor

--- cyanure_pytorch/erm/param/problem_param.py
+++ cyanure_pytorch/erm/param/problem_param.py
@@ -1,3 +1,7 @@
+import torch
+
+from cyanure_pytorch.constants import DEVICE
+
 class ProblemParameters:
     lambda_1: float
     lambda_2: float
@@ -6,10 +10,10 @@ class ProblemParameters:
     regul : str
     loss : str
 
-    def __init__(self, lambda_1: float = 0, lambda_2: float = 0, lambda_3: float = 0, intercept : bool = False, regul = None, loss="SQUARE"):
-        self.lambda_1 = lambda_1
-        self.lambda_2 = lambda_2
-        self.lambda_3 = lambda_3
+    def __init__(self, lambda_1: float = 0, lambda_2: float = 0, lambda_3: float = 0, intercept : bool = False, regul : str = "NONE", loss : str ="SQUARE"):
+        self.lambda_1 = torch.tensor([lambda_1], device=DEVICE)
+        self.lambda_2 = torch.tensor([lambda_2], device=DEVICE)
+        self.lambda_3 = torch.tensor([lambda_3], device=DEVICE)
         self.intercept = intercept
         self.regul = regul
         self.loss = loss
