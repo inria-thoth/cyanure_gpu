f72a2c8243922f699a8102b3ce2f14dd18a86e52
Thomas Ryckeboer <thomasryck@users.noreply.github.com>
2024-05-22 13:44:00 +0200

    chore: limit the torch version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='cyanure_pytorch',
       license='bsd-3-clause',
       url="https://inria-thoth.github.io/cyanure/welcome.html",
       description='optimization toolbox for machine learning',
-      install_requires=["scipy<=1.8.1;python_version<'3.11'", "scipy>=1.8.1;python_version>='3.11'", "numpy>=1.26.0;python_version>='3.11'", "numpy<=1.23.5;python_version<'3.11'",'scikit-learn', 'torch'],
+      install_requires=["scipy<=1.8.1;python_version<'3.11'", "scipy>=1.8.1;python_version>='3.11'", "numpy>=1.26.0;python_version>='3.11'", "numpy<=1.23.5;python_version<'3.11'",'scikit-learn', 'torch<2.3.0'],
       packages=find_packages(),
       cmdclass={'sdist': sdistzip},
       long_description="Cyanure is an open-source C++ software package with a Python 3 interface. The goal of Cyanure is to provide state-of-the-art solvers for learning linear models, based on stochastic variance-reduced stochastic optimization with acceleration mechanisms and Quasi-Newton principles. Cyanure can handle a large variety of loss functions (logistic, square, squared hinge, multinomial logistic) and regularization functions (l2, l1, elastic-net, fused Lasso, multi-task group Lasso). It provides a simple Python API, which should be fully compatible with scikit-learn.")
