0abe5f41f5309712240b57d78f42d183899065b9
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2024-10-04 10:59:15 +0200

    feat: Add precision variable

--- cyanure_pytorch/constants.py
+++ cyanure_pytorch/constants.py
@@ -1,6 +1,9 @@
 import torch
+import numpy as np
 
 EPSILON = 10e-10
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 NUMBER_OPTIM_PROCESS_INFO = 6
+TENSOR_TYPE = torch.float32
+ARRAY_TYPE = np.float32
