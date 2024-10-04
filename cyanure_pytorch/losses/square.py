6b00c149cdb3b45010498f4a11a7a3fc33a2b7f2
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2024-10-04 11:28:04 +0200

    fix: add precompute

--- cyanure_pytorch/losses/square.py
+++ cyanure_pytorch/losses/square.py
@@ -13,10 +13,24 @@ class SquareLoss(LinearLossVec):
         super().__init__(data, y, intercept)
         self.id = "SQUARE"
 
-    def eval_tensor(self, input: torch.Tensor) -> float:
-        tmp = self.pred_tensor(input)
+    def pre_compute(self, input: torch.Tensor) -> float:
+
+        tmp = self.pred_tensor(input, None)
+
         tmp = torch.sub(tmp, self.labels)
-        return 0.5*torch.linalg.norm(tmp)**2/tmp.size(dim=0)
+
+        return tmp
+
+    def eval_tensor(self, input: torch.Tensor, matmul_result: torch.Tensor = None, precompute: torch.Tensor = None) -> float:
+        if precompute is None:
+            if matmul_result is not None:
+                grad1 = matmul_result
+            else:
+                grad1 = self.pred_tensor(input, None)
+            grad1 = torch.sub(grad1, self.labels)
+        else:
+            grad1 = precompute
+        return 0.5*torch.linalg.norm(grad1)**2/grad1.size(dim=0)
 
     def eval(self, input: torch.Tensor, i: int) -> float:
         res = self.labels[i] - self.pred(i, input)
@@ -31,9 +45,16 @@ class SquareLoss(LinearLossVec):
     def scal_grad(self, input: torch.Tensor, i: int) -> float:
         return self.pred(i, input, None) - self.labels[i]
 
-    def get_grad_aux(self, input: torch.Tensor) -> torch.Tensor:
-        grad1 = self.pred_tensor(input)
-        grad1 = torch.sub(grad1, self.labels)
+    def get_grad_aux(self, input: torch.Tensor, matmul_result: torch.Tensor = None, precompute: torch.Tensor = None) -> torch.Tensor:
+
+        if precompute is None:
+            if matmul_result is not None:
+                grad1 = matmul_result
+            else:
+                grad1 = self.pred_tensor(input, None)
+            grad1 = torch.sub(grad1, self.labels)
+        else:
+            grad1 = precompute
 
         return grad1
 
