db068b7c27347a915572281804c7151dde142481
RYCKEBOER Thomas <37658138+thomasryck@users.noreply.github.com>
2024-10-25 12:09:33 +0200

    fix: change numpy version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(name='cyanure_pytorch',
       license='bsd-3-clause',
       url="https://inria-thoth.github.io/cyanure/welcome.html",
       description='optimization toolbox for machine learning',
-      install_requires=['scikit-learn', 'torch<2.3.0', 'numpy<2.0.0'],
+      install_requires=['scikit-learn', 'torch<2.3.0', 'numpy==1.26.4'],
       packages=find_packages(),
       cmdclass={'sdist': sdistzip},
       long_description="Cyanure is an open-source C++ software package with a Python 3 interface. The goal of Cyanure is to provide state-of-the-art solvers for learning linear models, based on stochastic variance-reduced stochastic optimization with acceleration mechanisms and Quasi-Newton principles. Cyanure can handle a large variety of loss functions (logistic, square, squared hinge, multinomial logistic) and regularization functions (l2, l1, elastic-net, fused Lasso, multi-task group Lasso). It provides a simple Python API, which should be fully compatible with scikit-learn.")
